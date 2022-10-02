ebcdaa43358c8dc5711844fe543ebe7972dc3efa
AidanQuigley <asq914@gmail.com>
2022-10-02 16:19:54 -0400

    commit2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import './App.css';
 import Body from './Body.jsx';
+import logo from './img/usync.png';
+import socEmail from './img/email.png';
+import socGoFundMe from './img/gofundme.png';
+import socInsta from './img/insta.png';
+import socTwitch from './img/twitch.png';
+import socTwitter from './img/twitter.png';
+import socYT from './img/yt.png';
+
 
 const App = () => {
     return (
         <div>
             <nav>
-                <img id='navLogo' src='../src/img/usync.png' />
+                <img id='navLogo' src={logo}/>
                 <ul id='navList'>
                     <li id='navHome'>Home</li>
                     <li id='navGames'>Games</li>
@@ -18,12 +26,12 @@ const App = () => {
                 <div>
                 <p style={{paddingTop: '20px'}}><a href='' style={{color: '#8D7DB5', textDecoration: 'underline'}}>Report A Problem</a></p>
                 <ul id="socials">
-                    <li><a href=''><img src='../src/img/twitter.png'/></a></li>
-                    <li><a href=''><img src='../src/img/insta.png'/></a></li>
-                    <li><a href=''><img src='../src/img/yt.png'/></a></li>
-                    <li><a href=''><img src='../src/img/twitch.png'/></a></li>
-                    <li><a href=''><img src='../src/img/gofundme.png'/></a></li>
-                    <li><a href=''><img src='../src/img/email.png'/></a></li>
+                    <li><a href=''><img src={socTwitter}/></a></li>
+                    <li><a href=''><img src={socInsta}/></a></li>
+                    <li><a href=''><img src={socYT}/></a></li>
+                    <li><a href=''><img src={socTwitch}/></a></li>
+                    <li><a href=''><img src={socGoFundMe}/></a></li>
+                    <li><a href=''><img src={socEmail}/></a></li>
                 </ul>
                 <p>usyncgg@gmail.com ©2022 by uSync LLC. <a href='' style={{color: 'white'}}>Contact Us</a></p>
                 <p>All content, games titles, trade names and/or trade dress, trademarks, 
