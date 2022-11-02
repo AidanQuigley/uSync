10252bdb742738f4af1679ce7f4a17cda95e7369
AidanQuigley <asq914@gmail.com>
2022-11-02 01:07:49 -0400

    navigation fix test 1

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,12 +11,12 @@ const Nav = () => {
                         <li id='navGames'>
                             <a href='../games'>Games</a>
                             <ul id='gamesDropDown'>
-                                <a href='../games/call-of-duty'><li className='ddGame'>Call of Duty</li></a>
-                                <a href='../games/warzone'><li className='ddGame'>Warzone</li></a>
-                                <a href='../games/league-of-legends'><li className='ddGame'>League of Legends</li></a>
-                                <a href='../games/halo'><li className='ddGame'>Halo</li></a>
-                                <a href='../games/rocket-league'><li className='ddGame'>Rocket League</li></a>
-                                <a href='../games/valorant'><li className='ddGame'>VALORANT</li></a>
+                                <a href='./games/call-of-duty'><li className='ddGame'>Call of Duty</li></a>
+                                <a href='./games/warzone'><li className='ddGame'>Warzone</li></a>
+                                <a href='./games/league-of-legends'><li className='ddGame'>League of Legends</li></a>
+                                <a href='./games/halo'><li className='ddGame'>Halo</li></a>
+                                <a href='./games/rocket-league'><li className='ddGame'>Rocket League</li></a>
+                                <a href='./games/valorant'><li className='ddGame'>VALORANT</li></a>
                             </ul>
                         </li>
                     <a href='../coming-soon'><li id='navComingSoon'>Coming Soon</li></a>
